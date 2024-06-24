8cb287d7b414eedfb9f75ff845c04b70367032bb
Morgan <me@morgan.kr>
2024-06-25 05:43:35 +0900

    Adding direct CSS due to SASS problem

--- src/lib/EditMessage.svelte
+++ src/lib/EditMessage.svelte
@@ -276,6 +276,15 @@
 
 </script>
 
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.css"
+    integrity="sha512-fHwaWebuwA7NSF5Qg/af4UeDx9XqUpYpOGgubo3yWu+b2IQR4UeQwbb42Ti7gVAjNtVoI/I9TEoYeu9omwcC6g=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer" />
+</svelte:head>
+
 <article
   id="{'message-' + message.uuid}"
   class="message chat-message" 
