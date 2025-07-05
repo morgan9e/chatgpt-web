f72e9402ab68916e6aa19db4cc72a2d2fd64f6d4
Morgan <me@morgan.kr>
2025-07-06 01:47:38 +0900

    Fix

--- src/lib/ChatOptionMenu.svelte
+++ src/lib/ChatOptionMenu.svelte
@@ -229,19 +229,12 @@
 
 <div class="dropdown {style}" class:is-active={showChatMenu} use:clickOutside={() => { showChatMenu = false }}>
   <div class="dropdown-trigger">
-    {#if !sidebarCollapsed }
     <button class="button is-ghost default-text" aria-haspopup="true" 
       aria-controls="dropdown-menu3" 
       on:click|preventDefault|stopPropagation={() => { showChatMenu = !showChatMenu }}
       >
       <span class="icon "><Fa icon={faEllipsis}/></span>
     </button>
-    {:else}
-      <div class="icon" aria-controls="dropdown-menu3" 
-        on:click|preventDefault|stopPropagation={() => { showChatMenu = !showChatMenu }}
-        >
-        <Fa icon={faEllipsis}/></div>
-    {/if}
   </div>
   <div class="dropdown-menu" id="dropdown-menu3" role="menu">
     <div class="dropdown-content">
